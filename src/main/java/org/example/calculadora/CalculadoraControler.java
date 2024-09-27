c3029159d5fcfa18d42f16c89c929688ea756374
Joelgs13 <050213jgonzalez@gmail.com>
2024-09-27 10:24:50 +0200

    Esqueleto integrado, y ajustados elementos del Controller

--- src/main/java/org/example/calculadora/CalculadoraControler.java
+++ src/main/java/org/example/calculadora/CalculadoraControler.java
@@ -1,4 +1,63 @@
 package org.example.calculadora;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
 public class CalculadoraControler {
+
+    @FXML
+    private Button calcular;
+
+    @FXML
+    private RadioButton dividir;
+
+    @FXML
+    private RadioButton multiplicar;
+
+    @FXML
+    private TextField o1;
+
+    @FXML
+    private TextField o2;
+
+    @FXML
+    private ToggleGroup operacion;
+
+    @FXML
+    private TextField res;
+
+    @FXML
+    private RadioButton restar;
+
+    @FXML
+    private RadioButton sumar;
+
+    @FXML
+    void calculo(ActionEvent event) {
+        double n2, n1;
+        try {
+            n2 = Double.parseDouble(o2.getText());
+            n1 = Double.parseDouble(o1.getText());
+        } catch (NumberFormatException e) {
+            System.out.println("No es un numero");
+            return;
+        }
+        if (sumar.isSelected()) {
+
+        } else if (restar.isSelected()) {
+
+        } else if (multiplicar.isSelected()) {
+
+        } else if (dividir.isSelected()) {
+             if(o2.getText()=="0") {
+
+             }
+        }
+    }
+
+
 }
