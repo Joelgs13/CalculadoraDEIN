a90ef8a47d0bc9c86e15e92d62fdb1a0a4af5a9a
Joelgs13 <050213jgonzalez@gmail.com>
2024-10-14 10:36:07 +0200

    hecho hasta ahora

--- src/main/java/org/example/calculadora/HelloApplication.java
+++ src/main/java/org/example/calculadora/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("CalculadoraControler.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 620, 340);
-        stage.setTitle("CalculadoraDejoel(MeQuieromatar)");
+        stage.setTitle("CalculadoraDejoel");
         stage.setScene(scene);
         stage.show();
     }
