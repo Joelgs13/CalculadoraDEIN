30201e8167850553f1b6adf96d164af111ffcf82
Joelgs13 <050213jgonzalez@gmail.com>
2024-11-12 15:07:49 +0100

    arreglado los 3 requerimientos, tal y como se me ha pedido.

--- src/main/java/org/example/calculadora/CalculadoraApp.java
+++ src/main/java/org/example/calculadora/CalculadoraApp.java
@@ -7,16 +7,33 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * Clase principal de la aplicación Calculadora.
+ * Extiende de {@link Application} y es el punto de entrada para lanzar la aplicación JavaFX.
+ */
 public class CalculadoraApp extends Application {
+
+    /**
+     * Metodo sobrescrito de la clase {@link Application} que inicializa y muestra la ventana principal de la aplicación.
+     *
+     * @param stage El escenario principal de JavaFX proporcionado por el sistema al lanzar la aplicación.
+     * @throws IOException Si ocurre un error al cargar el archivo FXML.
+     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CalculadoraApp.class.getResource("CalculadoraControler.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 620, 340);
         stage.setTitle("CalculadoraDejoel");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
+    /**
+     * Metodo principal de la aplicación. Llama a {@link Application#launch(String...)} para iniciar la aplicación JavaFX.
+     *
+     * @param args Argumentos de línea de comandos.
+     */
     public static void main(String[] args) {
         launch();
     }
--- src/main/java/org/example/calculadora/CalculadoraControler.java
+++ src/main/java/org/example/calculadora/CalculadoraControler.java
@@ -4,6 +4,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
+/**
+ * Controlador para la interfaz de la calculadora, que gestiona los eventos y operaciones
+ * aritméticas seleccionadas por el usuario.
+ */
 public class CalculadoraControler {
 
     @FXML
@@ -33,6 +37,12 @@ public class CalculadoraControler {
     @FXML
     private RadioButton sumar;
 
+    /**
+     * Realiza el cálculo de la operación aritmética seleccionada por el usuario.
+     * Este metodo se activa cuando el usuario hace clic en el botón "calcular".
+     *
+     * @param event El evento que dispara el cálculo, típicamente un clic en el botón "calcular".
+     */
     @FXML
     void calculo(ActionEvent event) {
         double n2 = 0, n1 = 0, resultado;
@@ -41,42 +51,41 @@ public class CalculadoraControler {
             n1 = Double.parseDouble(o1.getText());
         } catch (NumberFormatException e) {
             if (o2.getText().equals("") || o1.getText().equals("")) {
-                Alert alerta = new Alert (Alert.AlertType.ERROR);
+                Alert alerta = new Alert(Alert.AlertType.ERROR);
                 alerta.setHeaderText(null);
                 alerta.setTitle("Error");
-                alerta.setContentText("Espacio en blanco, escriba un numero en ambos operadores");
+                alerta.setContentText("Espacio en blanco, escriba un número en ambos operadores");
                 alerta.showAndWait();
             } else {
-                Alert alerta = new Alert (Alert.AlertType.ERROR);
+                Alert alerta = new Alert(Alert.AlertType.ERROR);
                 alerta.setHeaderText(null);
                 alerta.setTitle("Error");
-                alerta.setContentText("No es un numero valido");
+                alerta.setContentText("No es un número válido");
                 alerta.showAndWait();
             }
         }
-            if (sumar.isSelected()) {
-                resultado=n1+n2;
-                res.setText(""+resultado);
-            } else if (restar.isSelected()) {
-                resultado=n1-n2;
-                res.setText(""+(resultado));
-            } else if (multiplicar.isSelected()) {
-                resultado=n1*n2;
-                res.setText(""+(resultado));
-            } else if (dividir.isSelected()) {
-                resultado=n1/n2;
-                if(n2==0) {
-                    Alert alerta = new Alert (Alert.AlertType.ERROR);
-                    alerta.setHeaderText(null);
-                    alerta.setTitle("Error");
-                    alerta.setContentText("Divisor 0 localizado.");
-                    alerta.showAndWait();
-                    o2.setText("");
-                } else {
-                    res.setText(""+(resultado));
-                }
+
+        if (sumar.isSelected()) {
+            resultado = n1 + n2;
+            res.setText("" + resultado);
+        } else if (restar.isSelected()) {
+            resultado = n1 - n2;
+            res.setText("" + resultado);
+        } else if (multiplicar.isSelected()) {
+            resultado = n1 * n2;
+            res.setText("" + resultado);
+        } else if (dividir.isSelected()) {
+            resultado = n1 / n2;
+            if (n2 == 0) {
+                Alert alerta = new Alert(Alert.AlertType.ERROR);
+                alerta.setHeaderText(null);
+                alerta.setTitle("Error");
+                alerta.setContentText("Divisor 0 localizado.");
+                alerta.showAndWait();
+                o2.setText("");
+            } else {
+                res.setText("" + resultado);
             }
         }
     }
-
-
+}
